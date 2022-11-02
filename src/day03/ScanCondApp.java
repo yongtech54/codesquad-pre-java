ec0e6abb672384080b0c33d1d3f9101854cb819c
yongtech54 <yongtech54@gmail.com>
2022-11-02 12:36:03 +0900

    Refactoring: 부분 함수화 완료 - 기존 소스코드 함수화 진행하여 리팩토링

--- src/day03/ScanCondApp.java
+++ src/day03/ScanCondApp.java
@@ -7,8 +7,7 @@
 
 package day03;
 
-import java.sql.SQLOutput;
-import java.util.Scanner;
+import java.util.*;
 
 public class ScanCondApp {
     public static double ifCal(double num1, String op, double num2) {
@@ -36,15 +35,29 @@ public class ScanCondApp {
         }
     }
 
+    public static boolean zeroDiv(String op, double num2) {
+        if (op.equals("/") && num2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void printInfo() {
+        System.out.println("=====사칙연산 계산기 프로그램을 실행합니다=====");
+        System.out.println("프로그램의 연산방법을 선택하세요!");
+        System.out.println("0. 종료");
+        System.out.println("1. if-else문 함수 사용");
+        System.out.println("2. switch문 함수 사용");
+    }
+
+    public static void printResult(double num1, String op, double num2, double ans) {
+        System.out.println(num1 + " " + op + " " + num2 + " = " + ans);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            System.out.println("=====사칙연산 계산기 프로그램을 실행합니다=====");
-            System.out.println("프로그램의 연산방법을 선택하세요!");
-            System.out.println("0. 종료");
-            System.out.println("1. if-else문 함수 사용");
-            System.out.println("2. switch문 함수 사용");
+            printInfo();
 
             int choice = sc.nextInt();
             double num1, num2, ans;
@@ -56,9 +69,15 @@ public class ScanCondApp {
                 op = sc.next();
                 num2 = (double)sc.nextInt();
 
+                if (zeroDiv(op, num2)) {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    System.out.println("프로그램을 종료합니다.");
+                    break;
+                }
+
                 ans = ifCal(num1, op, num2);
 
-                System.out.println(num1 + " " + op + " " + num2 + " = " + ans);
+                printResult(num1, op, num2, ans);
 
             } else if (choice == 2) {
                 System.out.printf("식을 입력해주세요 (usage: 3 + 2) >> " );
@@ -66,8 +85,15 @@ public class ScanCondApp {
                 op = sc.next();
                 num2 = (double)sc.nextInt();
 
+                if (zeroDiv(op, num2)) {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    System.out.println("프로그램을 종료합니다.");
+                    break;
+                }
+
                 ans = switchCal(num1, op, num2);
-                System.out.println(num1 + " " + op + " " + num2 + " = " + ans);
+
+                printResult(num1, op, num2, ans);
 
             } else if (choice == 0){
                 System.out.println("프로그램을 종료합니다.");
