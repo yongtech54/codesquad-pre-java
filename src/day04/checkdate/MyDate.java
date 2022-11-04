0e12395accefc3766eb42b9b36586e80be5d4201
yongtech54 <yongtech54@gmail.com>
2022-11-05 00:45:55 +0900

    Create: 연월일 유효 판정 프로그램
    - 연월일 유효성 검사 구현
    - 윤년 검사 구현

--- src/day04/checkdate/MyDate.java
+++ src/day04/checkdate/MyDate.java
@@ -14,10 +14,14 @@
 
 package day04.checkdate;
 
+import java.util.Arrays;
+
 public class MyDate {
     private int day;
     private int month;
     private int year;
+    private int[] day31 = {1, 3, 5, 7, 8, 10, 12};
+    private int[] day30 = {4, 6, 9, 11};
 
     public MyDate(int inputDay, int inputMonth, int inputYear) {
         this.day = inputDay;
@@ -25,21 +29,78 @@ public class MyDate {
         this.year = inputYear;
     }
 
-    private boolean checkDate() {
+    private boolean day31Search() {
+        for (int i = 0; i < day31.length; i++) {
+            if (this.month == day31[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean day30Search() {
+        for (int i = 0; i < day30.length; i++) {
+            if (this.month == day30[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean fastCheck() {
         if (this.month < 1 || this.month > 12) {
             return false;
         }
         if (this.day < 1 || this.day > 31) {
             return false;
         }
-        if (this.month == 2 && this.day > 29) {
+        return true;
+    }
+
+    private boolean checkDay() {
+        if (day31Search() && this.day <= 31) {
+            return true;
+        }
+        if (day30Search() && this.day <= 30) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkFeburary() {
+        if (this.month == 2 && this.day <= 28) {
+            return true;
+        }
+        if (this.month == 2 && this.day == 29) {
+            if (checkLeapYear()) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    private boolean checkDate() {
+        if (!fastCheck()) {
+            return false;
+        }
+        if (this.month == 2) {
+            return checkFeburary();
+        }
+        if (!checkDay()) {
             return false;
         }
         return true;
     }
 
+    public boolean checkLeapYear() {
+        if ((this.year % 400 == 0) || ((this.year % 4 == 0) && (this.year % 100 != 0))) {
+            return true;
+        }
+        return false;
+    }
+
     public String isValid() {
-        if (this.checkDate()) {
+        if (checkDate()) {
             return this.year + "년 " + this.month + "월 " + this.day + "일은 "
                     + "유효한 날짜 입니다.";
         }
--- src/day04/checkdate/MyDateTest.java
+++ src/day04/checkdate/MyDateTest.java
@@ -4,7 +4,11 @@ public class MyDateTest {
     public static void main(String[] args) {
         MyDate date1 = new MyDate(30, 2, 2000);
         System.out.println(date1.isValid());
+
         MyDate date2 = new MyDate(2, 10, 2006);
         System.out.println(date2.isValid());
+
+        MyDate date3 = new MyDate(29, 2, 2024);
+        System.out.println(date3.isValid());
     }
 }
