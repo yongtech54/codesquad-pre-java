70e086cb9a96df634b32e1c0203d4135aa884b52
yongtech54 <yongtech54@gmail.com>
2022-11-05 13:41:15 +0900

    Modify: 생성자 오버라이딩 추가
    - 생성자 오버라이딩을 적용하여 다양한 객체 생성 가능하도록 구현
    - this()를 활용하여 가독성 개선

--- src/day04/calculator/CalculatorTest.java
+++ src/day04/calculator/CalculatorTest.java
@@ -2,9 +2,14 @@ package day04.calculator;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        Numbers number = new Numbers();
+        Numbers number1 = new Numbers();
 
-        Calculator.readyToCalculate(number);
+        Calculator.readyToCalculate(number1);
+        Calculator.printResults();
+
+        Numbers number2 = new Numbers(15, 5);
+
+        Calculator.readyToCalculate(number2);
         Calculator.printResults();
     }
 }
--- src/day04/calculator/Numbers.java
+++ src/day04/calculator/Numbers.java
@@ -1,6 +1,15 @@
 package day04.calculator;
 
 public class Numbers {
-    int num1 = 10;
-    int num2 = 2;
+    int num1;
+    int num2;
+
+    Numbers() {
+        this(10, 2);
+    }
+
+    Numbers(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
 }
