69c4cd1a6a4dfacc01acc88e713731d6ffea2e68
yongtech54 <yongtech54@gmail.com>
2022-11-05 13:22:29 +0900

    Modify: 참조변수 this 추가
    - 참조변수 this를 통해 가독성 높임

--- src/day04/introduce/HumanInfo.java
+++ src/day04/introduce/HumanInfo.java
@@ -1,18 +1,18 @@
 package day04.introduce;
 
 public class HumanInfo {
-    private String name;
-    private String gender = "여성";
-    private String isMarried = "기혼자";
-    private int numberOfChildren = 1;
-    private int age = 30;
+    String name;
+    String gender = "여성";
+    String isMarried = "기혼자";
+    int numberOfChildren = 1;
+    int age = 30;
 
-    public HumanInfo(String inputName) {
-        this.name = inputName;
+    HumanInfo(String name) {
+        this.name = name;
     }
 
     void introduce() {
-        System.out.println("이름은 " + name + "이고 나이는" + age + "입니다. "
+        System.out.println("이름은 " + name + "이고 나이는 " + age + "입니다. "
                             + "성별은 " + gender + "이며, " + isMarried + "입니다. "
                             + "자녀는 " + numberOfChildren + "명 있습니다.");
     }
