69e157f222274412903e712faf31b7fa42ec6de6
yongtech54 <yongtech54@gmail.com>
2022-11-09 19:50:45 +0900

    Create: [Programmers] - 12910번
    - 알고리즘 문제 풀이

---
+++ src/day08/Solution1.java
@@ -0,0 +1,44 @@
+package day08;
+
+class Solution1 {
+    public int[] solution(int[] arr, int divisor) {
+        // 배열 생성 및 초기화
+        int[] sample = new int[arr.length];
+        int cnt = 0;
+
+        for (int i = 0; i < sample.length; i++) {
+            if (arr[i] % divisor == 0) {
+                sample[cnt] = arr[i];
+                cnt++;
+            }
+        }
+
+        // 없는 경우
+        if (cnt == 0) {
+            int[] answer = {-1};
+            return answer;
+        }
+
+        // 정답 배열 생성 및 초기화
+        int[] answer = new int[cnt];
+
+        for (int i = 0; i < cnt; i++) {
+            answer[i] = sample[i];
+        }
+
+        // 오름차순 정렬
+        int temp;
+
+        for (int i = 0; i < cnt - 1; i++) {
+            for (int j = 0; j < cnt - 1; j++) {
+                if (answer[j] > answer[j + 1]) {
+                    temp = answer[j];
+                    answer[j] = answer[j + 1];
+                    answer[j + 1] = temp;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
